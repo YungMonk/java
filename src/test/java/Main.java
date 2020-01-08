9c5b99f18ea906fbce22161daa81ed28c96c59f9
xin.yang <xin.yang@ifchange.com>
2020-01-08 19:55:23 +0800

    自动化打包

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,6 +1,6 @@
-import lib.MysqlManager;
 import model.User;
 import model.UserBean;
+import lib.MysqlManager;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,14 +8,22 @@ import java.util.List;
 
 /**
  * @ClassName java.Main
- * @Description: 测试入口
+ * @Description: 测试包
  * @Author Yung
  * @Date 2020/1/3
  * @Version V1.0
  **/
 public class Main {
+    /**
+     * @MethodName: main
+     * @Description: 测试入口
+     * @Param: [args]
+     * @Return: void
+     * @Author: Yung
+     * @Date: 2020/1/8
+     **/
     public static void main(String[] args) throws SQLException {
-        List<User> list = new ArrayList<User>() {{
+        List<User> list = new ArrayList<User>(){{
             add(new User("王五", 1300));
             add(new User("朱六", 1200));
         }};
