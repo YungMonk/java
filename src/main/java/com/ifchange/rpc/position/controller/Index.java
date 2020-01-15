685dc28a28c5ba7432f801ae7c5dc9025cbcee86
xin.yang <xin.yang@ifchange.com>
2020-01-15 11:58:01 +0800

    web应用

--- src/main/java/com/ifchange/rpc/position/controller/Index.java
+++ src/main/java/com/ifchange/rpc/position/controller/Index.java
@@ -1,6 +1,5 @@
 package com.ifchange.rpc.position.controller;
 
-import com.ifchange.rpc.position.lib.MysqlManager;
 import com.ifchange.rpc.position.model.User;
 import com.ifchange.rpc.position.model.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,13 +21,11 @@ public class Index {
 
     @RequestMapping(value ="/index", method = RequestMethod.GET)
     @ResponseBody
-    public Map<?, ?> index() throws SQLException {
+    public Map<?, ?> index() {
         List<User> list = new ArrayList<User>() {{
             add(new User("Jon", 1300));
             add(new User("Tom", 1200));
         }};
-        // System.out.println(User.addUser(list));
-        // MysqlManager.close();
 
         HashMap<String, int[]> res = new HashMap<>();
         res.put("result", userBean.addUser(list));
