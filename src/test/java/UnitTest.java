e31127ab978257bb9356db034de3de6c0eeb57a9
xin.yang <xin.yang@ifchange.com>
2020-01-15 14:27:06 +0800

    Unit Test

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,4 +1,7 @@
 import com.ifchange.rpc.position.Application;
+import com.ifchange.rpc.position.lib.MysqlManager;
+import com.ifchange.rpc.position.model.User;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName SpringBootJdbcApplicationTests
@@ -17,12 +22,12 @@ import java.sql.SQLException;
  **/
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class SpringBootJdbcApplicationTests {
+public class UnitTest {
 
     @Autowired
     private DataSource dataSource;
 
-    @org.junit.Test
+    @Test
     public void testDataSource() throws SQLException {
 
         System.out.println("dataSource类型：" + dataSource.getClass());
@@ -30,5 +35,15 @@ public class SpringBootJdbcApplicationTests {
         Connection connection = dataSource.getConnection();
         System.out.println("connection连接：" + connection);
         connection.close();
+    }
+
+    @Test
+    public void testDataGeneral() throws SQLException {
+        List<User> list = new ArrayList<User>(){{
+            add(new User("王五", 1300));
+            add(new User("朱六", 1200));
+        }};
+        System.out.println(User.addUser(list));
+        MysqlManager.close();
     }
 }
