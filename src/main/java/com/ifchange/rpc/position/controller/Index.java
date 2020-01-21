f99d138cc436e4fc236c7ad626965d133a107980
xin.yang <xin.yang@ifchange.com>
2020-01-21 17:49:26 +0800

    redis.zset使用

--- src/main/java/com/ifchange/rpc/position/controller/Index.java
+++ src/main/java/com/ifchange/rpc/position/controller/Index.java
@@ -7,15 +7,14 @@ import com.ifchange.rpc.position.util.MRedis;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.DefaultTypedTuple;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/index")
@@ -47,6 +46,24 @@ public class Index {
     public ApiResult hello(Long id) {
         myRedis.set("sys:test_set_func", "isOK");
         myRedis.set("sys:dynamic_task", "0 0 * * * ?");
+
+        // 有序集合
+        Set<ZSetOperations.TypedTuple<String>> tuples = new HashSet<>();
+        tuples.add(new DefaultTypedTuple<String>("c", 3d));
+        tuples.add(new DefaultTypedTuple<String>("d", 4d));
+        tuples.add(new DefaultTypedTuple<String>("e", 5d));
+
+        myRedis.zAdd("sys:test_zset_add", tuples);
+
+        Set<String> set = myRedis.zRevRange("sys:test_zset_add", 0, -1);
+        System.out.println(set);
+
+        Set<ZSetOperations.TypedTuple<String>> zset = myRedis.zRangeWithScore("sys:test_zset_add", 0, -1);
+
+        for (ZSetOperations.TypedTuple<String> next : zset) {
+            System.out.println("value:" + next.getValue() + " score:" + next.getScore());
+        }
+
         return new ApiResult().success(new ArrayList<User>() {{
             add(new User("Jon", 1300));
             add(new User("Tom", 1200));
